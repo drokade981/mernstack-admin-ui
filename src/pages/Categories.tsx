bc16bc42dd2f03a02be0f53393a488e47a6de770
Devendra Rokade <drokade981@gmail.com>
2026-03-10 23:05:07 +0530

    add router

---
+++ src/pages/Categories.tsx
@@ -0,0 +1,10 @@
+function Categories() {
+  
+  return (
+    <>
+      <h1>Hello from categories</h1>
+    </>
+  )
+}
+
+export default Categories
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,10 @@
+function HomePage() {
+  
+  return (
+    <>
+      <h1>Hello from mernstack</h1>
+    </>
+  )
+}
+
+export default HomePage
